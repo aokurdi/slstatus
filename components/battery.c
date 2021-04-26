fd645a16755a090bcac6e9afcc5d35b4e2faac26
aokurdi <a.o.kurdi2@gmail.com>
2021-04-26 13:48:51 +0900

    Initial Commit

---
+++ components/battery.c
@@ -0,0 +1,312 @@
+/* See LICENSE file for copyright and license details. */
+#include <stdio.h>
+#include <string.h>
+
+#include "../util.h"
+
+#if defined(__linux__)
+	#include <limits.h>
+	#include <stdint.h>
+	#include <unistd.h>
+
+    int battery_state(const char *bat);
+
+	static const char *
+	pick(const char *bat, const char *f1, const char *f2, char *path,
+	     size_t length)
+	{
+		if (esnprintf(path, length, f1, bat) > 0 &&
+		    access(path, R_OK) == 0) {
+			return f1;
+		}
+
+		if (esnprintf(path, length, f2, bat) > 0 &&
+		    access(path, R_OK) == 0) {
+			return f2;
+		}
+
+		return NULL;
+	}
+
+	const char *
+	battery_perc(const char *bat)
+	{
+		int perc;
+		char path[PATH_MAX], *symbol;
+        char *bat_symbol[] = { "", "", "", "", "", "", "", "", "", "" };
+        char *bat_csymbol[] = { "", "", "", "", "", "", "" };
+
+		if (esnprintf(path, sizeof(path),
+		              "/sys/class/power_supply/%s/capacity", bat) < 0) {
+			return NULL;
+		}
+		if (pscanf(path, "%d", &perc) != 1) {
+			return NULL;
+		}
+
+        switch(battery_state(bat)){
+                case 0:
+                    switch(perc/10){
+                        case 0:
+                            symbol = bat_symbol[0];
+                            break;
+                        case 1:
+                            symbol = bat_symbol[1];
+                            break;
+                        case 2:
+                            symbol = bat_symbol[2];
+                            break;
+                        case 3:
+                            symbol = bat_symbol[3];
+                            break;
+                        case 4:
+                            symbol = bat_symbol[4];
+                            break;
+                        case 5:
+                            symbol = bat_symbol[5];
+                            break;
+                        case 6:
+                            symbol = bat_symbol[6];
+                            break;
+                        case 7:
+                            symbol = bat_symbol[7];
+                            break;
+                        case 8:
+                            symbol = bat_symbol[8];
+                            break;
+                        default:
+                            symbol = bat_symbol[9];
+                            break;
+                    }
+                    break;
+
+                case 1:
+                    switch(perc/10){
+                        case 0:
+                        case 1:
+                        case 2:
+                            symbol = bat_csymbol[0];
+                            break;
+                        case 3:
+                            symbol = bat_csymbol[1];
+                            break;
+                        case 4:
+                            symbol = bat_csymbol[2];
+                            break;
+                        case 5:
+                        case 6:
+                            symbol = bat_csymbol[3];
+                            break;
+                        case 7:
+                        case 8:
+                            symbol = bat_csymbol[4];
+                            break;
+                        case 9:
+                            symbol = bat_csymbol[5];
+                            break;
+                        default:
+                            symbol = bat_csymbol[6];
+                            break;
+                    }
+                    break;
+
+                default:
+                    return NULL;
+                };
+
+		return bprintf("%s %d", symbol, perc);
+	}
+
+    int
+	battery_state(const char *bat)
+	{
+		char path[PATH_MAX], state[12];
+
+		if (esnprintf(path, sizeof(path),
+		              "/sys/class/power_supply/%s/status", bat) < 0) {
+			return -1;
+		}
+		if (pscanf(path, "%12s", state) != 1) {
+			return -1;
+		}
+
+        return ((strcmp("Charging", state) != 0) ? 0 : 1);
+	}
+
+	const char *
+	battery_remaining(const char *bat)
+	{
+		uintmax_t charge_now, current_now, m, h;
+		double timeleft;
+		char path[PATH_MAX], state[12];
+
+		if (esnprintf(path, sizeof(path),
+		              "/sys/class/power_supply/%s/status", bat) < 0) {
+			return NULL;
+		}
+		if (pscanf(path, "%12s", state) != 1) {
+			return NULL;
+		}
+
+		if (!pick(bat, "/sys/class/power_supply/%s/charge_now",
+		          "/sys/class/power_supply/%s/energy_now", path,
+		          sizeof(path)) ||
+		    pscanf(path, "%ju", &charge_now) < 0) {
+			return NULL;
+		}
+
+		if (!strcmp(state, "Discharging")) {
+			if (!pick(bat, "/sys/class/power_supply/%s/current_now",
+			          "/sys/class/power_supply/%s/power_now", path,
+			          sizeof(path)) ||
+			    pscanf(path, "%ju", &current_now) < 0) {
+				return NULL;
+			}
+
+			if (current_now == 0) {
+				return NULL;
+			}
+
+			timeleft = (double)charge_now / (double)current_now;
+			h = timeleft;
+			m = (timeleft - (double)h) * 60;
+
+			return bprintf("%juh %jum", h, m);
+		}
+
+		return "";
+	}
+#elif defined(__OpenBSD__)
+	#include <fcntl.h>
+	#include <machine/apmvar.h>
+	#include <sys/ioctl.h>
+	#include <unistd.h>
+
+	static int
+	load_apm_power_info(struct apm_power_info *apm_info)
+	{
+		int fd;
+
+		fd = open("/dev/apm", O_RDONLY);
+		if (fd < 0) {
+			warn("open '/dev/apm':");
+			return 0;
+		}
+
+		memset(apm_info, 0, sizeof(struct apm_power_info));
+		if (ioctl(fd, APM_IOC_GETPOWER, apm_info) < 0) {
+			warn("ioctl 'APM_IOC_GETPOWER':");
+			close(fd);
+			return 0;
+		}
+		return close(fd), 1;
+	}
+
+	const char *
+	battery_perc(const char *unused)
+	{
+		struct apm_power_info apm_info;
+
+		if (load_apm_power_info(&apm_info)) {
+			return bprintf("%d", apm_info.battery_life);
+		}
+
+		return NULL;
+	}
+
+	const char *
+	battery_state(const char *unused)
+	{
+		struct {
+			unsigned int state;
+			char *symbol;
+		} map[] = {
+			{ APM_AC_ON,      "+" },
+			{ APM_AC_OFF,     "-" },
+		};
+		struct apm_power_info apm_info;
+		size_t i;
+
+		if (load_apm_power_info(&apm_info)) {
+			for (i = 0; i < LEN(map); i++) {
+				if (map[i].state == apm_info.ac_state) {
+					break;
+				}
+			}
+			return (i == LEN(map)) ? "?" : map[i].symbol;
+		}
+
+		return NULL;
+	}
+
+	const char *
+	battery_remaining(const char *unused)
+	{
+		struct apm_power_info apm_info;
+
+		if (load_apm_power_info(&apm_info)) {
+			if (apm_info.ac_state != APM_AC_ON) {
+				return bprintf("%uh %02um",
+			                       apm_info.minutes_left / 60,
+				               apm_info.minutes_left % 60);
+			} else {
+				return "";
+			}
+		}
+
+		return NULL;
+	}
+#elif defined(__FreeBSD__)
+	#include <sys/sysctl.h>
+
+	const char *
+	battery_perc(const char *unused)
+	{
+		int cap;
+		size_t len;
+
+		len = sizeof(cap);
+		if (sysctlbyname("hw.acpi.battery.life", &cap, &len, NULL, 0) == -1
+				|| !len)
+			return NULL;
+
+		return bprintf("%d", cap);
+	}
+
+	const char *
+	battery_state(const char *unused)
+	{
+		int state;
+		size_t len;
+
+		len = sizeof(state);
+		if (sysctlbyname("hw.acpi.battery.state", &state, &len, NULL, 0) == -1
+				|| !len)
+			return NULL;
+
+		switch(state) {
+			case 0:
+			case 2:
+				return "+";
+			case 1:
+				return "-";
+			default:
+				return "?";
+		}
+	}
+
+	const char *
+	battery_remaining(const char *unused)
+	{
+		int rem;
+		size_t len;
+
+		len = sizeof(rem);
+		if (sysctlbyname("hw.acpi.battery.time", &rem, &len, NULL, 0) == -1
+				|| !len
+				|| rem == -1)
+			return NULL;
+
+		return bprintf("%uh %02um", rem / 60, rem % 60);
+	}
+#endif
